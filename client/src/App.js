9df583d3e066081102fa1d82be0473661f70bb98
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-05-01 19:37:11 +0300

    basic styling for all application and react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/home/Home";
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
